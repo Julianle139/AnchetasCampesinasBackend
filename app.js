9785fd573c95fe4f7a1f966af0d0a011ee265646
Julian Gallo <julianleonardogallo@gmail.com>
2021-11-08 15:56:50 -0500

    Prueba V1.4 F

--- app.js
+++ app.js
@@ -39,36 +39,3 @@ app.use("/api/ventas",ventasRoutes);
 module.exports = app;
 
 //mongodb+srv://Admin:<password>@cluster0.t6b1o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
-//const userDB = "dbUserApp";
-//const passDB = "QsSK8JN1shm2neR6";
-//const dBase = "GestionSanVicente";
-//const productsRoutes = require("./routes/products");
-//const categoriaRoutes = require("./routes/categoria");
-///const usuarioRoutes = require("./routes/usuarios");
-//const ventasRoutes = require("./routes/ventas");
-//app.use(cors());
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-//app.use(cors());
-
-//mongoose
-//  .connect(
-//    "mongodb+srv://" +
-//      userDB +
-//      ":" +
-//      passDB +
-//      "@sanvicente.l1mmx.mongodb.net/" +
-//      dBase +
-//      "?retryWrites=true&w=majority"
-//  )
-//  .then(() => {
-//    console.log("Estamos conectados");
-//  });
-
-//app.use("/api/products", productsRoutes);
-//app.use("/api/categoria", categoriaRoutes);
-//app.use("/api/usuarios",usuarioRoutes);
-//app.use("/api/ventas",ventasRoutes);
-
-//module.exports = app;
--- models/ventas.js
+++ models/ventas.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const venta = mongoose.Schema({
   producto: { type: String},
   cliente: { type: String },
-  idcliente: { type: String },
+  idCliente: { type: String },
   vendedor:{type:String},
   precio: { type: Number},
   cantidad: {type: Number}
