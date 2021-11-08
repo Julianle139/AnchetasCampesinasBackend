9153aab87019cb6a900f70766360a929d7ccdd07
Julian Gallo <julianleonardogallo@gmail.com>
2021-11-08 09:00:00 -0500

    Backend

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var mongoose = require("mongoose");
 const cors = require("cors");
 var app = express();
 
-const userDB = "dbUserApp";
-const passDB = "QsSK8JN1shm2neR6";
-const dBase = "GestionSanVicente";
+const userDB = "Admin";
+const passDB = "admin123";
+const dBase = "myFirstDatabase";
 
 const productsRoutes = require("./routes/products");
 const categoriaRoutes = require("./routes/categoria");
@@ -23,7 +23,7 @@ mongoose
       userDB +
       ":" +
       passDB +
-      "@sanvicente.l1mmx.mongodb.net/" +
+      "@cluster0.t6b1o.mongodb.net/" +
       dBase +
       "?retryWrites=true&w=majority"
   )
@@ -37,3 +37,41 @@ app.use("/api/usuarios",usuarioRoutes);
 app.use("/api/ventas",ventasRoutes);
 
 module.exports = app;
+
+//mongodb+srv://Admin:<password>@cluster0.t6b1o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+
+//const userDB = "dbUserApp";
+//const passDB = "QsSK8JN1shm2neR6";
+//const dBase = "GestionSanVicente";
+
+//const productsRoutes = require("./routes/products");
+//const categoriaRoutes = require("./routes/categoria");
+///const usuarioRoutes = require("./routes/usuarios");
+//const ventasRoutes = require("./routes/ventas");
+
+//app.use(cors());
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(cors());
+
+//mongoose
+//  .connect(
+//    "mongodb+srv://" +
+//      userDB +
+//      ":" +
+//      passDB +
+//      "@sanvicente.l1mmx.mongodb.net/" +
+//      dBase +
+//      "?retryWrites=true&w=majority"
+//  )
+//  .then(() => {
+//    console.log("Estamos conectados");
+//  });
+
+//app.use("/api/products", productsRoutes);
+//app.use("/api/categoria", categoriaRoutes);
+//app.use("/api/usuarios",usuarioRoutes);
+//app.use("/api/ventas",ventasRoutes);
+
+//module.exports = app;
